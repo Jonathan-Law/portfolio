168102fbf2fd956dadfc3000eebac12028a7b79e
Jonathan Law <jonathan.law@domo.com>
2016-10-21 23:09:28 -0600

    fix(): fix for title for routes

--- src/responsive/responsive.init.js
+++ src/responsive/responsive.init.js
@@ -1,7 +1,16 @@
 module.exports = init;
 
-function init() {
+function init($rootScope) {
 	// Add any app initialization code here.
+  $rootScope.$on('$stateChangeSuccess', (event, toState /*, toParams, fromState, fromParams*/) => {
+    // toState === $state.current
+    if (toState.name === 'main') {
+      $rootScope.title = 'JonLaw.us';
+    } else if (toState.name === 'tiffiny') {
+      $rootScope.title = 'Tiffiny Law';
+    }
+    // $rootScope.title = toState.name;
+  });
 }
 
-init.$inject = [];
+init.$inject = ['$rootScope'];
